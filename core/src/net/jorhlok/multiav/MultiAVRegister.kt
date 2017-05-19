9ba4561941bf7a6f33da2cdb24a1cf3ed608bca4
Jorhlok <joshmbrown42@gmail.com>
2017-05-18 20:58:30 -0500

    Experimenting with Kotlin

---
+++ core/src/net/jorhlok/multiav/MultiAVRegister.kt
@@ -0,0 +1,207 @@
+package net.jorhlok.multiav
+
+import com.badlogic.gdx.graphics.g2d.Animation
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.math.Vector2
+import java.util.HashMap
+
+/**
+ * Multi-Function Audio-Visual Organization Engine
+ * @author Jorhlok
+ */
+class MultiAVRegister {
+    protected var Image: MutableMap<String, TexGrid>
+    protected var Frame: MutableMap<String, SprFrame>
+    protected var Anim: MutableMap<String, AnimSeq>
+    protected var Letters: MutableMap<Char, TextureRegion>
+    protected var SFX: MutableMap<String, SEffect>
+    protected var Mus: MutableMap<String, MTrack>
+    protected var Font: BitmapFont? = null //assumes monospace font
+    protected var DrawableChars: String? = null
+
+    var batch: Batch? = null
+    var scale = Vector2(1f,1f)
+    var camPos = Vector2(0f,0f)
+    var tabLength = 4f
+    var fontSampling = 1f
+
+    init {
+        Image = HashMap<String, TexGrid>()
+        Frame = HashMap<String, SprFrame>()
+        Anim = HashMap<String, AnimSeq>()
+        Letters = HashMap<Char, TextureRegion>()
+        SFX = HashMap<String, SEffect>()
+        Mus = HashMap<String, MTrack>()
+    }
+
+    fun newImage(key: String, uri: String, tw: Int, th: Int) {
+        val i = Image[key]
+        i?.dispose()
+        Image.put(key, TexGrid(key, uri, tw, th))
+    }
+
+    fun newSprite(key: String, img: String, tx: Int, ty: Int, tw: Int, th: Int, hf: Boolean, vf: Boolean) {
+        Frame.put(key, SprFrame(key, img, tx, ty, tw, th, hf, vf))
+    }
+
+    fun newAnim(key: String, frames: Array<String>, speed: Float, mode: Animation.PlayMode?) {
+        Anim.put(key, AnimSeq(key, frames, speed, mode))
+    }
+
+    fun setFont(f: BitmapFont, chars: String) {
+        if (Font != null) Font!!.dispose()
+        Font = f
+        DrawableChars = chars
+    }
+
+    fun newSFX(key: String, uri: String) {
+        val s = SFX[key]
+        s?.dispose()
+        SFX.put(key, SEffect(key, uri))
+    }
+
+    fun newMusic(key: String, uri: String) {
+        val m = Mus[key]
+        m?.dispose()
+        Mus.put(key, MTrack(key, uri))
+    }
+
+    fun newMusic(key: String, intro: String, body: String) {
+        val m = Mus[key]
+        m?.dispose()
+        Mus.put(key, MTrack(key, intro, body))
+    }
+
+    fun Generate() {
+        for (t in Image.values)
+            t.Generate()
+        for (s in Frame.values) {
+            s.Generate(Image)
+            newAnim("_" + s.Name, arrayOf(s.Name), 0f, null) //auto generate single frame animation for each frame
+        }
+        for (a in Anim.values)
+            a.Generate(Frame)
+        if (Font != null && DrawableChars != null && !DrawableChars!!.isEmpty()) {
+            val dat = Font!!.data
+            for (i in 0..DrawableChars!!.length - 1) {
+                val glyph = dat.getGlyph(DrawableChars!![i])
+                if (glyph != null) {
+                    Letters.put(DrawableChars!![i], TextureRegion(Font!!.getRegion(glyph.page).texture, glyph.u, glyph.v, glyph.u2, glyph.v2))
+                }
+            }
+        }
+        for (s in SFX.values)
+            s.Generate()
+    }
+
+    fun drawRegion(reg: TextureRegion, x: Float, y: Float, sw: Float, sh: Float, rot: Float) {
+        //For some reson was drawing with width and height swapped and rotated 90 degrees when done normally
+        batch?.draw(reg, x, y, 0f, 0f, reg.regionHeight.toFloat() * scale.y * sh, reg.regionWidth.toFloat() * scale.x * sw, 1f, 1f, rot - 90, false)
+
+
+    }
+
+    @JvmOverloads fun draw(anim: String, statetime: Float, x: Float, y: Float, sw: Float = 1f, sh: Float = 1f, rot: Float = 0f) {
+        var x = x
+        var y = y
+        try {
+            val reg = Anim[anim]?.getKeyFrame(statetime)
+            if (reg != null) {
+                x += (reg.regionHeight.toDouble() * scale.y.toDouble() * sh.toDouble() * Math.sin(Math.toRadians((-1 * rot).toDouble()))).toFloat()
+                y += (reg.regionHeight.toDouble() * scale.y.toDouble() * sh.toDouble() * Math.cos(Math.toRadians((-1 * rot).toDouble()))).toFloat()
+                drawRegion(reg, x, y, sw, sh, rot)
+            }
+        } catch (e: Exception) {
+            //nothing
+        }
+
+    }
+
+    @JvmOverloads fun drawString(str: String, x: Float, y: Float, sw: Float = 1f, sh: Float = 1f, rot: Float = 0f) {
+        var x = x
+        var y = y
+        x /= 2f
+        y /= 2f
+        val w = Font!!.spaceWidth
+        val h = Font!!.lineHeight
+        x += (h.toDouble() * scale.y.toDouble() * sh.toDouble() * 0.5 * Math.sin(Math.toRadians((-1 * rot).toDouble())) / fontSampling).toFloat()
+        y += (h.toDouble() * scale.y.toDouble() * sh.toDouble() * 0.5 * Math.cos(Math.toRadians((-1 * rot).toDouble())) / fontSampling).toFloat()
+        val xtrav = w.toDouble() * Math.cos(Math.toRadians(rot.toDouble())) * sw.toDouble() * scale.x.toDouble() / fontSampling
+        val ytrav = w.toDouble() * Math.sin(Math.toRadians(rot.toDouble())) * sw.toDouble() * scale.y.toDouble() / fontSampling
+        val xjmp = h.toDouble() * Math.sin(Math.toRadians(rot.toDouble())) * sh.toDouble() * scale.x.toDouble() / fontSampling
+        val yjmp = h.toDouble() * Math.cos(Math.toRadians(rot.toDouble())) * sh.toDouble() * scale.y.toDouble() / fontSampling
+
+        var xcur = x.toDouble()
+        var ycur = y.toDouble()
+        var linesdown = 0
+        for (i in 0..str.length - 1) {
+            val c = str[i]
+            when (c) {
+                '\n' -> {
+                    ++linesdown
+                    xcur = x + xjmp * linesdown
+                    ycur = y - yjmp * linesdown
+                }
+                '\t' -> {
+                    xcur += xtrav * tabLength
+                    ycur += ytrav * tabLength
+                }
+                else -> {
+                    val reg = Letters[c]
+                    val xoff = (Font!!.data.getGlyph(c).xoffset / w).toDouble()
+                    val yoff = (Font!!.data.getGlyph(c).yoffset / h).toDouble()
+                    if (reg != null)
+                        drawRegion(reg, x + (xcur + xoff * xtrav - yoff * xjmp).toFloat(), y + (ycur + xoff * ytrav + yoff * yjmp).toFloat(), sw / fontSampling, sh * -1 / fontSampling, rot)
+                    xcur += xtrav
+                    ycur += ytrav
+                }
+            }
+        }
+    }
+
+    fun getSFX(key: String): SEffect? {
+        return SFX[key]
+    }
+
+    fun getMus(key: String): MTrack? {
+        return Mus[key]
+    }
+
+    fun playSFX(key: String) {
+        SFX[key]?.play()
+    }
+
+    fun setSFXVolume(v: Float) {
+        for (s in SFX.values)
+            s.setVolume(v)
+    }
+
+    fun setMusVolume(v: Float) {
+        for (m in Mus.values)
+            m.setVolume(v)
+    }
+
+    fun killMusic() {
+        for (m in Mus.values) {
+            m.stop()
+            m.dispose()
+        }
+    }
+
+    fun dispose() {
+        Font?.dispose()
+        batch?.dispose()
+        for (t in Image.values)
+            t.dispose()
+        for (s in Frame.values)
+            s.dispose()
+        for (a in Anim.values)
+            a.dispose()
+        for (s in SFX.values)
+            s.dispose()
+        for (m in Mus.values)
+            m.dispose()
+    }
+}
---
+++ core/src/net/jorhlok/shootyshoot/ShootyShoot.kt
@@ -0,0 +1,157 @@
+package net.jorhlok.shootyshoot
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Animation
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.Hinting
+import com.badlogic.gdx.maps.tiled.TmxMapLoader
+import com.badlogic.gdx.math.Vector2
+import net.jorhlok.multiav.MultiAVRegister
+import net.jorhlok.oops.ObjectOrientedPlaySet
+
+
+class ShootyShoot
+    (private var oops: ObjectOrientedPlaySet = ObjectOrientedPlaySet(),
+     private var mav: MultiAVRegister = MultiAVRegister()) : ApplicationAdapter() {
+    private val camera = OrthographicCamera()
+    private val campos = Vector2()
+
+
+    override fun create() {
+//        mav = MultiAVRegister()
+        mkav()
+        mav.Generate()
+        mav.batch = SpriteBatch()
+        camera.setToOrtho(false, (640 / 16).toFloat(), (360 / 16).toFloat())
+        campos.x = (640 / 32).toFloat()
+        camera.position.x = campos.x
+        campos.y = (360 / 32).toFloat()
+        camera.position.y = campos.y
+        mav.scale = Vector2(1f / 16, 1f / 16)
+        mav.camPos = campos
+        mav.setMusVolume(0.5f)
+
+//        oops = ObjectOrientedPlaySet()
+        oops.setMAV(mav)
+        oops.addTileMap("test0", TmxMapLoader(InternalFileHandleResolver()).load("map/test0.tmx"))
+        oops.addTileMap("test1", TmxMapLoader(InternalFileHandleResolver()).load("map/test1.tmx"))
+        oops.addEntityType("testplat", TestPlatformer::class.java)
+
+        val dm = TestDM("test1", null)
+        dm.cam = camera
+        oops.addMasterScript("testdm", dm)
+        oops.launchScript("testdm")
+
+        System.out.println("hullo from kotlin")
+    }
+
+
+    override fun render() {
+        Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        val deltatime = Gdx.graphics.deltaTime
+
+        //game logic
+        oops.step(deltatime)
+
+        camera.update()
+        mav.batch?.projectionMatrix = camera.combined
+        try {
+            oops.draw(deltatime)
+        } catch (e: Exception) {
+            System.err.println("Error drawing!")
+            e.printStackTrace()
+        }
+
+    }
+
+
+    override fun resize(width: Int, height: Int) {
+
+    }
+
+
+    override fun dispose() {
+        oops.dispose()
+        mav.dispose()
+    }
+
+
+    fun mkav() {
+        mav.newMusic("frcasio", "bgm/FriendlyCasiotone.ogg")
+        mav.newMusic("mkds", "bgm/mkdsintro.ogg", "bgm/mkds.ogg")
+
+        mav.newSFX("pew", "sfx/pew.wav")
+        mav.newSFX("jump", "sfx/jump.wav")
+        mav.newSFX("dash", "sfx/dash.wav")
+        mav.newSFX("growl", "sfx/growl.wav")
+
+        val generator = FreeTypeFontGenerator(Gdx.files.internal("gfx/libmono.ttf"))
+        val parameter = FreeTypeFontGenerator.FreeTypeFontParameter()
+        parameter.size = 14 * 6
+        parameter.genMipMaps = true
+        parameter.magFilter = Texture.TextureFilter.Nearest
+        parameter.minFilter = Texture.TextureFilter.Linear
+        //parameter.characters = parameter.characters + "▄■";
+        parameter.hinting = Hinting.Full
+        mav.setFont(generator.generateFont(parameter), parameter.characters)
+        mav.fontSampling = 6f
+        generator.dispose()
+
+        mav.newImage("sprites", "gfx/sprites.png", 16, 16)
+
+        mav.newSprite("guyrt", "sprites", 0, 0, 1, 1, false, false)
+        mav.newSprite("guylf", "sprites", 0, 0, 1, 1, true, false)
+        mav.newSprite("gunrt", "sprites", 1, 0, 1, 1, false, false)
+        mav.newSprite("gunlf", "sprites", 1, 0, 1, 1, true, false)
+        mav.newSprite("pac0lf", "sprites", 14, 0, 1, 1, false, false)
+        mav.newSprite("pac0rt", "sprites", 14, 0, 1, 1, true, false)
+        mav.newSprite("pac1lf", "sprites", 15, 0, 1, 1, false, false)
+        mav.newSprite("pac1rt", "sprites", 15, 0, 1, 1, true, false)
+
+        mav.newSprite("shot0", "sprites", 0, 1, 1, 1, false, false)
+        mav.newSprite("shot1", "sprites", 1, 1, 1, 1, false, false)
+        mav.newSprite("shot2", "sprites", 2, 1, 1, 1, false, false)
+        mav.newSprite("shot3", "sprites", 3, 1, 1, 1, false, false)
+
+        mav.newSprite("door", "sprites", 0, 2, 1, 1, false, false)
+        mav.newSprite("pedestal", "sprites", 1, 2, 1, 1, false, false)
+        mav.newSprite("alchemy", "sprites", 2, 2, 2, 1, false, false)
+
+        mav.newSprite("mineral", "sprites", 0, 3, 1, 1, false, false)
+        mav.newSprite("herb", "sprites", 1, 3, 1, 1, false, false)
+        mav.newSprite("eyeball", "sprites", 2, 3, 1, 1, false, false)
+        mav.newSprite("worm", "sprites", 3, 3, 1, 1, false, false)
+        mav.newSprite("dollar", "sprites", 4, 3, 1, 1, false, false)
+        mav.newSprite("potion", "sprites", 5, 3, 1, 1, false, false)
+        mav.newSprite("mana", "sprites", 6, 3, 1, 1, false, false)
+        mav.newSprite("coffee", "sprites", 7, 3, 1, 1, false, false)
+
+        mav.newSprite("redbar", "sprites", 0, 4, 1, 1, false, false)
+        mav.newSprite("darkredbar", "sprites", 1, 4, 1, 1, false, false)
+        mav.newSprite("bluebar", "sprites", 2, 4, 1, 1, false, false)
+        mav.newSprite("darkbluebar", "sprites", 3, 4, 1, 1, false, false)
+        mav.newSprite("yellowbar", "sprites", 4, 4, 1, 1, false, false)
+        mav.newSprite("darkyellowbar", "sprites", 5, 4, 1, 1, false, false)
+        mav.newSprite("uiback", "sprites", 6, 4, 1, 1, false, false)
+        mav.newSprite("uislot", "sprites", 7, 4, 1, 1, false, false)
+        mav.newSprite("cursor", "sprites", 8, 4, 1, 1, false, false)
+
+        mav.newSprite("greenblock", "sprites", 0, 15, 1, 1, false, false)
+        mav.newSprite("skyblueblock", "sprites", 1, 15, 1, 1, false, false)
+        mav.newSprite("whiteblock", "sprites", 2, 15, 1, 1, false, false)
+        mav.newSprite("greyblock", "sprites", 3, 15, 1, 1, false, false)
+
+        mav.newAnim("newshot", arrayOf("shot0", "shot1", "shot2"), 0.125f, Animation.PlayMode.NORMAL)
+        mav.newAnim("shot", arrayOf("shot3", "shot2"), 0.01f, Animation.PlayMode.LOOP)
+        mav.newAnim("paclf", arrayOf("pac0lf", "pac1lf"), 0.5f, Animation.PlayMode.LOOP)
+        mav.newAnim("pacrt", arrayOf("pac0rt", "pac1rt"), 0.5f, Animation.PlayMode.LOOP)
+    }
+}
